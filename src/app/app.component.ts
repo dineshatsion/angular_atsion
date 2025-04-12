b97704a27f82c00088d05ad8f4fcd69d5d76c186
Prasanna-342 <laxmipotharaju139@gmail.com>
2025-04-12 17:23:33 +0530

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { Navbar1Component } from './navbar1/navbar1.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule,FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
